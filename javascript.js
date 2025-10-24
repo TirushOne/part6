7e1ec94476ee13f1b7d2dc0d1092ab3e36a720a5
Jed McKenna <jedmckenna63@gmail.com>
2025-10-24 13:29:15 +1100

    fixed highlighting

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let searchBox = document.getElementById("search");
+let searchIdButton = document.getElementById("search-id");
+let searchTitleButton = document.getElementById("search-title");
+
 class Movie {
     constructor(id, title, year, rating) {
         this.id = id;
@@ -17,108 +21,66 @@ class MovieList {
         this.movies.push(movie);
     }
 
-    sort() {
-        let radioButtons = document.querySelectorAll('input[name="sort-type"]');
-        let selectedValue;
-        
-        for (const radioButton of radioButtons) {
-            if (radioButton.checked) {
-                selectedValue = radioButton.value;
-                break;
-            }
-        }
+    prepareMovies() {
 
-        console.log(selectedValue);
-        
-        switch (selectedValue) {
-            case "A-Z":
-                this.order = "A-Z";
-                this.movies.sort((a, b) => a.title.localeCompare(b.title));
-                console.log(this.movies);
-                break;
-            case "Z-A":
-                this.order = "Z-A";
-                this.movies.sort((a, b) => b.title.localeCompare(a.title));
-                console.log(this.movies);
-                break;
-            case "best-movies":
-                this.order = "best";
-                this.movies.sort((a, b) => a.title.localeCompare(b.title));
-                console.log(this.movies);
-                break;
-        }
     }
 
-    writeDisplay() {
-        function updateMovieDisplay(movies) {
-            let display = document.getElementById("movies");
-            
-            while (display.lastChild) {
-                display.removeChild(display.lastChild);
-            }
-            
-            movies.forEach(movie => {
-                function createInfoField(card, name, content, className) {
-                    let nameElement = document.createElement("span");
-                    nameElement.textContent = `${name}: `;
-                    
-                    card.appendChild(nameElement);
-                    let span = document.createElement("span");
-                    span.className = className;
-                    span.textContent = content;
-                    
-                    card.appendChild(span);
-                    card.appendChild(document.createElement('br'));
+    updateDisplay() {
+        //making these values a constants might allow the js runtime to
+        //hoist the switch and if statements that use these values
+        const sortOrder = getSelectedSortOrder();
+
+        const searchValue = searchBox.value;
+
+        const searchType = (searchValue && searchValue != "") ? getSelectedSearchType() : null;
+
+        writeMovieDisplay(
+            this.movies.filter(movie => {
+                switch (searchType) {
+                    case "id":
+                        return movie.id == searchValue;
+                    case "title":
+                        return movie.title.includes(searchValue);
+                    default:
+                        return true;
                 }
-        
-                let card = document.createElement("div");
-                card.className = "movie-card";
-        
-                let title = document.createElement("h4");
-                title.textContent = movie.title;
-        
-                if (movie.highlights?.title) {
-                    title.className = "highlight";
+            }).map((movie) => { 
+                let highlights = {};
+    
+                switch (sortOrder) {
+                    case "A-Z":
+                        highlights.title = true;
+                    break;
+                    case "Z-A":
+                        highlights.title = true;
+                    break;
+                    case "best-movies":
+                        highlights.rating = true;
+                    break;
                 }
-        
-                card.appendChild(title);
-        
-                createInfoField(card, "Year of release", movie.year, (movies.highlights?.year) ? "highlight" : "");
-                createInfoField(card, "Rating", movie.rating, (movies.highlights?.rating) ? "highlight" : "");
-                createInfoField(card, "Id", movie.id, (movies.highlights?.rating) ? "highlight" : "");
-        
-                display.appendChild(card);
-            });
-        }
 
-        let mapped_movies = this.movies.map((movie) => { 
-            let highlights;
-
-            switch (this.order) {
-                case "A-Z":
-                    highlights = {
-                        "title":true
-                    };
-                break;
-                case "Z_A":
-                    highlights = {
-                        "title":true
-                    };
-                break;
-                default:
-                    highlights = {
-                        "rating":true
-                    };
-                break;
-            }
-
-            movie.highlights = highlights;
-            return movie;
-        });
-
-        updateMovieDisplay(
-            mapped_movies
-        )
+                switch (searchType) {
+                    case "id":
+                        highlights.id = true;
+                    break;
+                    case "title":
+                        highlights.title = true;
+                    break;
+                }
+    
+                movie.highlights = highlights;
+                return movie;
+            }).sort((a, b) => {
+                switch (sortOrder) {
+                    case "A-Z":
+                        return a.title.localeCompare(b.title);
+                    case "Z-A":
+                        return b.title.localeCompare(a.title);
+                    case "best-movies":
+                        return a.rating.localeCompare(b.rating);
+                }
+            })
+        );
     }
 
     gatherMovieAdd() {
@@ -159,23 +121,108 @@ class MovieList {
 
 }
 
+function writeMovieDisplay(movies) {
+    let display = document.getElementById("movies");
+    
+    while (display.lastChild) {
+        display.removeChild(display.lastChild);
+    }
+    
+    movies.forEach(movie => {
+        function createInfoField(card, name, content, className) {
+            let nameElement = document.createElement("span");
+            nameElement.textContent = `${name}: `;
+            
+            card.appendChild(nameElement);
+            let span = document.createElement("span");
+            span.className = className;
+            span.textContent = content;
+            
+            card.appendChild(span);
+            card.appendChild(document.createElement('br'));
+        }
+
+        let card = document.createElement("div");
+        card.className = "movie-card";
+
+        let title = document.createElement("h4");
+        title.textContent = movie.title;
+
+        if (movie.highlights?.title) {
+            title.className = "highlight";
+        }
+
+        card.appendChild(title);
+
+        createInfoField(card, "Year of release", movie.year, (movie.highlights?.year) ? "highlight" : "");
+        createInfoField(card, "Rating", movie.rating, (movie.highlights?.rating) ? "highlight" : "");
+        createInfoField(card, "Id", movie.id, (movie.highlights?.id) ? "highlight" : "");
+
+        display.appendChild(card);
+    });
+}
+
+function getSelectedSortOrder() {
+    let radioButtons = document.querySelectorAll('input[name="sort-type"]');
+    let selectedValue;
+        
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedValue = radioButton.value;
+            break;
+        }
+    }
+
+    return selectedValue;
+}
+
+function getSelectedSearchType() {
+    return (searchIdButton.checked) ? "id" : "title";
+}
+
+function sortMovies(movies) {
+    switch (getSelectedSortOrder()) {
+        case "A-Z":
+            movies.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+        case "Z-A":
+            movies.sort((a, b) => b.title.localeCompare(a.title));
+        break;
+        case "best-movies":
+            movies.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+    }
+
+    return movies;
+}
+
+
+
 let movieList = new MovieList();
 
 document.getElementById("add-movie").addEventListener("submit", (e) => {
     e.preventDefault();
     movieList.gatherMovieAdd();
-    movieList.sort();
-    movieList.writeDisplay();
+    movieList.updateDisplay();
 });
 
 document.querySelectorAll('input[name="sort-type"]').forEach(button => {
     button.addEventListener("change", (e) => {
-        console.log("changed");
         e.preventDefault();
-        movieList.sort();
-        movieList.writeDisplay();
+        movieList.updateDisplay();
     });
 });
+
+document.querySelectorAll('input[name="search-type"]').forEach(button => {
+    button.addEventListener("change", (e) => {
+        e.preventDefault();
+        movieList.updateDisplay();
+    });
+});
+
+searchBox.addEventListener("change", (e) => {
+    movieList.updateDisplay();
+})
 
 
 
